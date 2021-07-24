cda9c27fd6093e8191ed522cd73a2a821a00db70
myller-silva <myllersilva1310@gmail.com>
2021-07-24 20:20:54 -0300

    Create q03.py

---
+++ Fundamentos-da-Programacao/Lista02/q03.py
@@ -0,0 +1,6 @@
+# Faça um algoritmo que leia um valor inteiro e verifica se o valor é par ou ímpar, retornando um valor booleano.
+
+n = int(input("Digite um numero inteiro: ")) 
+t = True if (n%2==0)  else False
+
+print(f"{n} é par ? {t}")
