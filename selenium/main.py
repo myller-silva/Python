c193ac2ada18f5e696de626dc75da1e6687aa3aa
Myller Silva <myllersilva1310@gmail.com>
2022-05-22 09:28:01 -0300

    Update main.py

--- selenium/main.py
+++ selenium/main.py
@@ -1,18 +1,20 @@
 from selenium import webdriver
 
-# abrir navegador:
+url = "https://centralnovel.com/series/supreme-magus/"
+
+vol7_xpath = '//*[@id="post-8353"]/div[4]/div[4]/div/ul'
+
+
 # navegadorChrome = webdriver.Chrome()
-navegadorEdge = webdriver.Edge()
-# navegar para pagina:
-navegadorEdge.get("https://centralnovel.com/series/supreme-magus/")
+navegadorEdge = webdriver.Edge() # abrir navegador
 
-xpath = '//*[@id="post-8353"]/div[4]/div[4]/div/ul'
-vol7 = navegadorEdge.find_element_by_xpath(xpath)
+navegadorEdge.maximize_window()
 
-lis = vol7.find_elements_by_class_name("dlpdf")
-print(f"lis: {len(lis)}")
+navegadorEdge.get(url) # navegar para pagina
+
+vol7 = navegadorEdge.find_element_by_xpath(vol7_xpath)
 
 
-navegadorEdge.close()
+# navegadorEdge.close()
 
 # 125
