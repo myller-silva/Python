613e35d8de360b6c66f7d18b56970bc681372d12
Myller Silva <myllersilva1310@gmail.com>
2022-05-22 10:08:44 -0300

    Update main.py

--- selenium/main.py
+++ selenium/main.py
@@ -2,11 +2,11 @@ from selenium import webdriver
 
 url = "https://centralnovel.com/series/supreme-magus/"
 
-navegadorEdge = webdriver.Edge() # abrir navegador
+navEdge = webdriver.Edge() # abrir navegador
 
-navegadorEdge.get(url) # navegar para pagina
+navEdge.get(url) # navegar para pagina
 
-volumes = navegadorEdge.find_elements_by_class_name("eplister")
+volumes = navEdge.find_elements_by_class_name("eplister")
 
 pdfsVol7 = volumes[2].find_elements_by_class_name("dlpdf")
 
@@ -14,4 +14,6 @@ length = len(pdfsVol7)
 
 for i in range(0, 2):
   href = pdfsVol7[i].get_property("href")
-  navegadorEdge.execute_script(f"window.open('{href}', '_blank')")
+  navEdge.execute_script(f"window.open('{href}', '_blank')")
+
+navEdge.close()
