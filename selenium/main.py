0324b6d2d1bb11ff8fb890113b46d2fef36a31d2
Myller Silva <myllersilva1310@gmail.com>
2022-05-22 13:55:23 -0300

    Update main.py

--- selenium/main.py
+++ selenium/main.py
@@ -1,37 +1,20 @@
 from selenium import webdriver
-url = "https://centralnovel.com/series/supreme-magus/"
-
-navEdge = webdriver.Edge() # abrir navegador
-
-navEdge.get(url) # navegar para pagina
-
-volumes = navEdge.find_elements_by_class_name("eplister")
-
-pdfsVol7 = volumes[2].find_elements_by_class_name("dlpdf")
-
-length = len(pdfsVol7)
-print(f"length: {length}")
-
-primeiro = 619
-ultimo = len(pdfsVol7)
-
-for i in range(primeiro, ultimo+1):
-  print()
+from time import sleep
+navEdge = webdriver.Edge()  # abrir navegador
 
 
+c = 629
+f = 744
 
+navEdge.get(f"https://centralnovel.com/supreme-magus-capitulo-{c}/pdf/")  # navegar para pagina
+c+=1
 
+for i in range(c, f+1):
+  url = f'https://centralnovel.com/supreme-magus-capitulo-{i}/pdf/'
+  # navEdge.get(url)  # navegar para pagina
+  navEdge.execute_script(f"window.open('{url}', '_blank')")
+  sleep(5)
 
-# for i in range(0, 3):
-#   href = pdfsVol7[i].get_property("href")
-#   numCap = href.__str__()
-#   numCap = numCap[numCap.find("capitulo", 0) : ]
-#   numCap = numCap[numCap.find("-", 0)+1 : ]
-#   numCap = numCap[: numCap.find("/", 0) ]
+sleep(60)
 
-#   print(f"str: {numCap}")
-#   print(f"href: {href}")
-
-
-# navEdge.close()
-  # navEdge.execute_script(f"window.open('{href}', '_blank')")
+navEdge.close()
