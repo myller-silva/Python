43c3ebda801ce04f383cfdd3c2b6b2ec37dac393
Myller Silva <myllersilva1310@gmail.com>
2022-05-22 10:55:21 -0300

    update

--- selenium/main.py
+++ selenium/main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 
+
+
 url = "https://centralnovel.com/series/supreme-magus/"
 
 navEdge = webdriver.Edge() # abrir navegador
@@ -12,8 +14,16 @@ pdfsVol7 = volumes[2].find_elements_by_class_name("dlpdf")
 
 length = len(pdfsVol7)
 
-for i in range(0, 2):
+for i in range(0, 3):
   href = pdfsVol7[i].get_property("href")
-  navEdge.execute_script(f"window.open('{href}', '_blank')")
+  numCap = href.__str__()
+  numCap = numCap[numCap.find("capitulo", 0) : ]
+  numCap = numCap[numCap.find("-", 0)+1 : ]
+  numCap = numCap[: numCap.find("/", 0) ]
+
+  print(f"str: {numCap}")
+  print(f"href: {href}")
+
 
 navEdge.close()
+  # navEdge.execute_script(f"window.open('{href}', '_blank')")
--- testes.py
+++ testes.py
@@ -1,8 +1,1 @@
-def condicao(str):
-  print(str)
-  return True
-  
-if(not condicao("1") or condicao("2")):
-  print("ola mundo")
-else:
-  print("else")
+
