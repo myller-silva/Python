a2e2449eb03cff0bb596f874d68c19888effe58e
Myller Silva <myllersilva1310@gmail.com>
2022-05-22 09:15:26 -0300

    Update q3.py

--- Fundamentos-da-Programacao/monitoria/lista06/q3.py
+++ Fundamentos-da-Programacao/monitoria/lista06/q3.py
@@ -8,16 +8,16 @@ def printMatriz(matriz, n, format):
 
 def ehQuadradoMagico(matriz, n):
     chave = sum(matriz[0: n*n: n+1])  # diagonal principal
-    if(sum(matriz[n-1: n*n-1: n-1]) != chave):
-      return False  # diagonal secundaria
+    if(sum(matriz[n-1: n*n-1: n-1]) != chave): # diagonal secundaria
+      return False  
     for i in range(0, n):
-      if(sum(matriz[i*n: i*n+n: 1]) != chave or sum(matriz[i: n*n: n]) != chave):
-        return False  # linhas ou colunas
+      if(sum(matriz[i*n: i*n+n: 1]) != chave or sum(matriz[i: n*n: n]) != chave): # linhas ou colunas
+        return False  
     return True
 
 
-matriz = []
-n = 3  # exemplo de quadrado magico: [4, 9, 2, 3, 5, 7, 8, 1, 6]
+matriz = [] # exemplo de quadrado magico: [4, 9, 2, 3, 5, 7, 8, 1, 6]
+n = 3 
 for i in range(0, n*n):
   matriz.append(randint(0, 9))
 if(n*n != len(matriz) or n == 1):
