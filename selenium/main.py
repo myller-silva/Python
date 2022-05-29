f64b7ce226b04250075ea9a20431884648a94c40
Myller Silva <myllersilva1310@gmail.com>
2022-05-29 15:51:58 -0300

    Update main.py

--- selenium/main.py
+++ selenium/main.py
@@ -3,18 +3,17 @@ from time import sleep
 navEdge = webdriver.Edge()  # abrir navegador
 
 
-c = 629
-f = 744
+c = 771
+f = 1083
 
 navEdge.get(f"https://centralnovel.com/supreme-magus-capitulo-{c}/pdf/")  # navegar para pagina
 c+=1
+sleep(5)
 
 for i in range(c, f+1):
   url = f'https://centralnovel.com/supreme-magus-capitulo-{i}/pdf/'
-  # navEdge.get(url)  # navegar para pagina
-  navEdge.execute_script(f"window.open('{url}', '_blank')")
+  navEdge.execute_script(f"window.open('{url}', '_self')")
   sleep(5)
-
 sleep(60)
 
 navEdge.close()
